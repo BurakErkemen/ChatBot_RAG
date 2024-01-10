57c8a1c61c4f19baf67b16d6a0460e627c522a7f
BurakErkemen <burakfurkanerkemen@gmail.com>
2024-01-10 05:47:23 +0300

    Update Web Scraping

--- Web_Scraping/Web_Scraping.py
+++ Web_Scraping/Web_Scraping.py
@@ -5,12 +5,47 @@ from selenium.webdriver.common.by import By
 import requests
 import pandas as pd 
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+
 browser = webdriver.Edge()
+dizi=[]
+duyurular = "https://yazilimtf.firat.edu.tr/tr/announcements-all"
 
-duyuru1 = "https://yazilimtf.firat.edu.tr/tr/announcements-all"
+browser.get(duyurular)
+sleep(5)
 
-browser.get(duyuru1)
+height = 0 
+for i in range(1,13):
+    git_button = browser.find_element(By.XPATH, f"/html/body/div[2]/div[1]/div[1]/a[{i}]")
+    sleep(3)
+    git_button.click()
+    sleep(3)
+    browser.execute_script("window.scrollTo(0,200)")
+    title = browser.find_element(By.CSS_SELECTOR,"new-section-detail-title")
+    date = browser.find_element(By.CSS_SELECTOR,"new-section-detail-date")
+    context = browser.find_element(By.CSS_SELECTOR,"new-section-detail-explanation")
+    item = {
+        'baslık': title,
+        'icerik': context,
+        'tarih': date
+    }
+    dizi.append(item)
+    browser.back()
+    height +=192
+    browser.execute_script(f"window.scrollTo(0,{height})") #3267
+    sleep(3)
 
+sleep(25)
 
-sleep(3)
+print(dizi)
 
+sleep(25)
+browser.quit()
