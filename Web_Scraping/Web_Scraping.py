6d6ce8b4d26abc34d4e0f1ae85f11037db366296
BurakErkemen <burakfurkanerkemen@gmail.com>
2024-01-12 06:41:37 +0300

    Update Scrapping

--- Web_Scraping/Web_Scraping.py
+++ Web_Scraping/Web_Scraping.py
@@ -4,15 +4,11 @@ from time import sleep
 from selenium.webdriver.common.by import By
 import requests
 import pandas as pd 
-
+from fpdf import FPDF
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
 
 browser = webdriver.Edge()
@@ -20,7 +16,7 @@ dizi=[]
 
 sleep(2)
 """
-for i in range(1,67):
+for i in range(1,6):
     height = 0 
     new_url = f"https://yazilimtf.firat.edu.tr/tr/announcements-all/{i}"
     browser.get(new_url)
@@ -32,8 +28,9 @@ for i in range(1,67):
         sleep(5)
         browser.execute_script("window.scrollTo(0,200)")
         title = browser.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div[2]/div[3]/h3").text
-        date = browser.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div[2]/div[2]/p").text
         context = browser.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div[2]/div[4]/p[2]").text
+        date = browser.find_element(By.XPATH,"/html/body/div[2]/div/div[1]/div[2]/div[2]/p").text
+        
         item = {
             'baslık': title,
             'icerik': context,
@@ -45,33 +42,45 @@ for i in range(1,67):
         browser.execute_script(f"window.scrollTo(0,{height})") #3267
         sleep(3)
 """
+
 akademisyen = "https://yazilimtf.firat.edu.tr/academic-staffs"
-browser.execute_script("window.scrollTo(0,200)")
+#browser.execute_script("window.scrollTo(0,200)")
 
 browser.get(akademisyen)
-sleep(3)
-#for i in range(1,21):
-link = browser.find_element(By.XPATH,f"/html/body/div[3]/div/div/div[2]/div/div/div[1]/div[2]/div[2]/div[1]/div[4]/a")
-height_akademic = 0
-link.click()
 sleep(5)
-unvan = browser.find_element(By.XPATH,"/html/body/main/div[3]/div[1]/div[2]/div[1]") 
-ad = browser.find_element(By.XPATH,"/html/body/main/div[3]/div[1]/div[2]/div[2]/h2")
-kurum = browser.find_element(By.XPATH,"/html/body/main/div[3]/div[1]/div[2]/p")
-uzmanlık = browser.find_element(By.XPATH,'//*[@id="uzmanlik_text"]')
-yok_ıd = browser.find_element(By.XPATH,'//*[@id="yok_academic_id_text"]')
-browser.execute_script("window.scrollTo(0,3200)")
-mail = browser.find_element(By.XPATH,"/html/body/main/div[3]/div[3]/p[3]/a")
-item = {
-            'unvan': unvan,
-            'ad': ad,
-            'kurum': kurum,
-            'uzmanlık ':uzmanlık,
-            'yok_ıd' : yok_ıd,
-            'mail':mail
-        }
-dizi.append(item)
 
+def clean_text(text):
+    return ''.join(char for char in text if char.isprintable())
+
+pdf = FPDF()
+pdf.add_page()
+pdf.add_font("ArialUnicode", fname=r"C:\Users\burak\Downloads\ArialUnicodeMSRegular\ArialUnicodeMSRegular.ttf")
+pdf.set_font("ArialUnicode", size=12)  # Bu satır eklenmiş
+
+for i in range(20):
+    try:
+        Ad = browser.find_elements(By.CSS_SELECTOR, ".personnel-card-info-name")[i].text
+        Mail = browser.find_elements(By.CSS_SELECTOR, ".personnel-card-info-contact-mail")[i].text
+        CalısmaAlan = browser.find_elements(By.CSS_SELECTOR, ".personnel-card-info-work-places")[i].text
+        item = {
+            'ad': Ad,
+            'mail': Mail,
+            'calısmalanları': CalısmaAlan
+        }
+        dizi.append(item)
+
+        cleaned_ad = clean_text(Ad)
+        cleaned_mail = clean_text(Mail)
+        cleaned_calisma_alanlari = clean_text(CalısmaAlan)
+
+        text = f"Ad: {cleaned_ad}\nMail: {cleaned_mail}\nÇalışma Alanları: {cleaned_calisma_alanlari}\n\n"
+        pdf.cell(200, 10, txt=text, ln=True)
+    except IndexError:
+        print(f"IndexError: {i} numaralı eleman bulunamadı.")
+
+pdf.output("output.pdf", "F")
+
+print("PDF oluşturuldu.")
 print(dizi)
-sleep(5)
+
 browser.quit()
